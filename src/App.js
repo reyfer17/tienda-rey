58fc20cc4c795bd9c02e69a456892c9c9b039835
Reyfer17 <reyfer17@gmail.com>
2022-08-30 22:58:01 -0300

    creacion de nav-bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <p class="navbar-brand" href="#">TIENDA REY</p>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <p class="nav-link active" href="#">Principal
+            <span class="visually-hidden">(actual)</span>
+          </p>
+        </li>
+        <li class="nav-item">
+          <p class="nav-link" href="#">Libros</p>
+        </li>
+        <li class="nav-item">
+          <p class="nav-link" href="#">Juguetes</p>
+        </li>
+        <li class="nav-item">
+          <p class="nav-link" href="#">Accesorios</p>
+        </li>
+        <li class="nav-item dropdown">
+          <p class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Ropa</p>
+          <div class="dropdown-menu">
+            <p class="dropdown-item" href="#">Calzado</p>
+            <p class="dropdown-item" href="#">Pantalones/Shorts/Calzas</p>
+            <p class="dropdown-item" href="#">Remeras/Camisas/Sacos/Buzos</p>
+            <div class="dropdown-divider"></div>
+          </div>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-sm-2" type="text" placeholder="Search"></input>
+        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Busqueda</button>
+      </form>
     </div>
+  </div>
+</nav>
+
   );
 }
 
