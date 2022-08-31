695f94781adb51b98aee4467146f3c522d33691c
Reyfer17 <reyfer17@gmail.com>
2022-08-30 23:29:47 -0300

    aplicados ajustes por css al navbar

--- src/App.js
+++ src/App.js
@@ -2,47 +2,20 @@ import "./App.css"
 
 const App = () => {
   return (
-
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <div class="container-fluid">
-    <p class="navbar-brand" href="#">TIENDA REY</p>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarColor01">
-      <ul class="navbar-nav me-auto">
-        <li class="nav-item">
-          <p class="nav-link active" href="#">Principal
-            <span class="visually-hidden">(actual)</span>
-          </p>
-        </li>
-        <li class="nav-item">
-          <p class="nav-link" href="#">Libros</p>
-        </li>
-        <li class="nav-item">
-          <p class="nav-link" href="#">Juguetes</p>
-        </li>
-        <li class="nav-item">
-          <p class="nav-link" href="#">Accesorios</p>
-        </li>
-        <li class="nav-item dropdown">
-          <p class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Ropa</p>
-          <div class="dropdown-menu">
-            <p class="dropdown-item" href="#">Calzado</p>
-            <p class="dropdown-item" href="#">Pantalones/Shorts/Calzas</p>
-            <p class="dropdown-item" href="#">Remeras/Camisas/Sacos/Buzos</p>
-            <div class="dropdown-divider"></div>
-          </div>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-sm-2" type="text" placeholder="Search"></input>
-        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Busqueda</button>
-      </form>
-    </div>
-  </div>
-</nav>
-
+    <header>
+      <div id="title">TIENDA REY</div>
+      <div>
+        <nav >
+          <ul>
+            <li>Inicio</li>
+            <li>Libros</li>
+            <li>Juguetes</li>
+            <li>Ropa</li>
+            <li>Accesorios</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 }
 
